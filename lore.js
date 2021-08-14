3b56b974f636251d8bf749e4af04e5292f70b466
Maxwell Zen <maxwell.zen@gmail.com>
2021-08-13 23:22:05 -0400

    Finished lore specific page setup

--- lore.js
+++ lore.js
@@ -1,5 +1,7 @@
 
 var spanclasses = ["h c1", "v c1", "h c2", "v c2", "h c3", "v c3", "h c4", "v c4"];
+var defaultinfo = ["CATEGORY", "DOMAIN", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"];
+var imgname = ["resources/Materna Icon.svg"];
 
 function plainborder(div) {
 	var node;
