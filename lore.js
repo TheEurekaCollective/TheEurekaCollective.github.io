8de06a47d6fedcd3fdc70529a5f7bb52df70b907
Maxwell Zen <maxwell.zen@gmail.com>
2021-08-23 16:52:02 -0400

    removed initlorespecific function, changed how links are set

--- lore.js
+++ lore.js
@@ -5,6 +5,7 @@ var loretxt = ["Antimatter is the primary form of energy storage within the Huma
 "In the Great Cities, it is not uncommon for an aspiring couple to walk into a Materna clinic and request a BioTube reservation. For what you may ask? For a baby! In the 26th century, nearly 93% of all births take place within an artificial uterus...",
 "Humanity has long mastered the human body. Genetic modification and alteration are the norm. One can get a cell sculpt from the corner store. An entire field of fashion known as fleshcraft sprung from this newfound ability. It..."];
 var loreimg = ["resources/Antimatter Icon.svg", "resources/Materna Icon.svg", "resources/Madame Martins Icon.svg"];
+var lorespecificlink = ['lorespecific', 'lorespecific', 'lorespecific'];
 
 function initlorepage() {
 	var submenu = document.querySelectorAll("a.submenup");
@@ -43,7 +44,7 @@ function initlorepage() {
 			div.append(node);
 			node = document.createElement('a');
 			node.className = "entrylink";
-			node.href = "lorespecific.html?id=" + i;
+			node.href = lorespecificlink[i];
 			div.append(node);
 			feed.append(div);
 			i += 1;
@@ -93,74 +94,6 @@ function filter(c) {
 		if (i >= loreimg.length) {
 			document.getElementsByClassName("lorefeed")[0].style.height = (y+24)+'vw';
 			break;
-		}
-	}
-}
-
-
-
-var defaultinfo = ["CATEGORY", "DOMAIN", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"];
-var imgname = ["resources/Materna Icon.svg"];
-var boxinfo = ["CATEGORY--Factions|Human Domain|Corporation|Human|Synthia Faber|September 1st, 2021"];
-var article = [`===MATERNA
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-==SECTION TITLE
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-=Paragraph Title
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`]
-
-
-
-function initlorespecific() {
-	var queryString = window.location.search;
-	var urlParams = new URLSearchParams(queryString);
-	var id = parseInt(urlParams.get('id'));
-	document.querySelectorAll("img.infoimg")[0].src=imgname[id];
-	// alert(document.querySelectorAll("img.infoimg")[0].src);
-	var info = boxinfo[id].split('|');
-	for (var i = 0; i < 6; i++) {
-		var a = "", b = "";
-		var index = info[i].indexOf("--");
-		if (index==-1) {
-			a=defaultinfo[i];
-			b=info[i];
-		} else {
-			a = info[i].substring(0, index);
-			b = info[i].substring(index+2, info[i].length);
-		}
-		document.getElementById("boxh"+i).textContent = a;
-		document.getElementById("boxi"+i).textContent = b;
-	}
-	var div = document.getElementsByClassName("specificall")[0];
-	var text = article[id].split('\n');
-	for (var i = 0; i < text.length; i++) {
-		if (text[i].length >= 3 && text[i].substring(0, 3)=="===") {
-			var node = document.createElement('p');
-			node.className = "infoheading";
-			node.textContent = text[i].substring(3, text[i].length);
-			div.append(node);
-			node = document.createElement('span');
-			node.className = "underline";
-			div.append(node);
-		} else if (text[i].length >= 2 && text[i].substring(0, 2)=='==') {
-			var node = document.createElement('p');
-			node.className = "sectiontitle";
-			node.textContent = text[i].substring(2, text[i].length);
-			div.append(node);
-			node = document.createElement('span');
-			node.className = "underline";
-			div.append(node);
-		} else if (text[i].length >= 1 && text[i].substring(0, 1)=='=') {
-			var node = document.createElement('p');
-			node.className = "paragraphtitle";
-			node.textContent = text[i].substring(1, text[i].length);
-			div.append(node);
-		} else {
-			var node = document.createElement('p');
-			node.className = "infoparagraph";
-			node.textContent = text[i];
-			div.append(node);
 		}
 	}
 }
