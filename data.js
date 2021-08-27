39388e00311ae23853ac82a94c3b4b14092f294e
Maxwell Zen <maxwell.zen@gmail.com>
2021-08-27 01:28:00 -0400

    Results page javascript works, still need to implement submenu

--- data.js
+++ data.js
@@ -8,10 +8,41 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 `AETERNUM
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in velit esse cillum dolore eu fugiat quis nostrud exercitation ullamco laboris nulla pariatur. Duis nulla pariatur. Duis aute irure dolor in reprehenderit in velit esse cillum dolore eu fugiat quis nostrud exercitation fugiat quis nostrud exercitation ullamco nulla pariatur.`]
 const pagetitle = ["MATERNA", "AETERNUM"];
-const pagelink = ["lorespecific.html?id=0", "artspecific.html"];
+const pagelink = ["lorespecific.html", "artspecific.html"];
+const type = ["lore", "art"];
+const indices = [1, 0];
+var loreindices;
+var artindices;
 
 for (var i = 0; i < pagedata.length; i++) {
 	pagedata[i] = pagedata[i].toLowerCase();
+}
+
+function initresults() {
+	var queryString = window.location.search;
+	var urlParams = new URLSearchParams(queryString);
+	var search = urlParams.get("search");
+	loreindices = [];
+	artindices = [];
+	if (search.length==0) {
+		return;
+	}
+	var tosort = [];
+	for (var i = 0; i < pagedata.length; i++) {
+		tosort.push([score(i, search), i]);
+	}
+	tosort.sort(function(a, b) {return a[0]-b[0];});
+	tosort.reverse();
+	for (var i = 0; i < tosort.length; i++) {
+		if (tosort[i][0]>0) {
+			var x = tosort[i][1];
+			if (type[x] == "lore") {
+				loreindices.push(indices[x]);
+			} else {
+				artindices.push(indices[x]);
+			}
+		}
+	}
 }
 
 
