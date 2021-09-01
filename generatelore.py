1fd2effdf8879ae30ad08547862ff78cabca02c3
Maxwell Zen <maxwell.zen@gmail.com>
2021-08-31 23:05:43 -0400

    Added Human Thaumatology lore

--- generatelore.py
+++ generatelore.py
@@ -2,27 +2,27 @@
 
 # This will be displayed as the title of the tab, ex "Aeternum"
 # will display the title "Aeternum - Project Anima" on the tab
-tabtitle = "Human Spells"
+tabtitle = "Human Thaumatology"
 
-keywords = "​​​​​​spells, thaumaturgy, psions, mana , emitters, Ancient Thaumaturgy, formulas, chants, Modern Thaumaturgy, neural extension, cognition improvement, ritual, neural interface, psion generation, spell collection, Spell Compendium, Fusion Point, Atmospheric Repulsion, Material Compression, hyperelements, Ionize, Hardlight, lightspeed weapons, Psionic Interference, static spells, scalar spells, casting"
+keywords = "​​​​​​arcane arts, magus cabals, alchemists, mages, thaumatologists, thaumaturgy, Ancient Thaumatology, Verses, Division historians, black powder, Modern Thaumatology, conceptual particles, Lifted Veil Event, Thaumaturgical Revolution, Johannes Siddhar, United States’ Bureau of Paraphysics, Esoteric Energies and Their Properies, mana, Solana Igarashi, Theory of Conceptual Particle Interactions, USSR’s State Research Center of Koltsovo, infons, psions, Terran Accords, Theory of Thaumatology, Paraphysics Division, Human Domain, magic, conceptual particles, esoteric energies, life extension, mana production rate, mana pool, neural networking, cognition improvement, brain-computer interfaces, group casting, emitter, wands, somatic threading, spell composition"
 
 # This is the information that will appear in the lil box on
 # the right, the order the entries appear in the box is:
 # 1 4
 # 2 5
 # 3 6
-infoheadings = ["CATEGORY", "DOMAIN", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"]
-infoentries = ["Subjects", "Human Domain", "Thaumatology", "Human", "Devin Deng", "September 1st, 2021"]
+infoheadings = ["CATEGORY", "LOCATION", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"]
+infoentries = ["History", "Human Domain", "Thaumatology", "Human", "Devin Deng", "September 1st, 2021"]
 
 # this is the address of the image that appears in the lil
 # box on the right, make sure it's relative to the html file
 # (probably something like resources/image.jpg)
-infoimagesource = "resources/Human Spells Icon.svg"
+infoimagesource = "resources/Human Thaumatology Icon.svg"
 
 # if the article has links, put the addresses for those links
 # here. They must be listed in the same order as they appear
 # in the article. You can omit the .html extension
-articlelinks = ['paraphysics-lore#thaumatology', 'humanthaumatology-lore#ancient-thaumatology', 'humanthaumatology-lore#modern-thaumatology']
+articlelinks = ['paraphysics-lore#thaumatology', 'humanspells-lore']
 
 # this is the hardest part so read it carefully, look at the
 # example for reference, make sure text is surrounded by triple quotes.
@@ -49,43 +49,28 @@ articlelinks = ['paraphysics-lore#thaumatology', 'humanthaumatology-lore#ancient
 # 6 - any line that doesn't start with equal signs is interpreted
 #     as a regular paragraph
 
-article = """===HUMAN SPELLS
-Spells are the foundation of {thaumaturgy}, serving as which thaumaturgical phenomena are generated. They are energy-information packages composed of psions and mana, cast from devices known as emitters.
-==HISTORY
-During the age of {Ancient Thaumatology}, methods of spell composition and casting often took the form of various written formulas and verbal chants. Thaumaturgists of this time believed that the formulas and chants themselves caused the phenomena to occur, leading to inconsistent or inefficient casting. In reality, the formulas and chants guide the caster’s thoughts to produce the thaumaturgical phenomenon. This was later realized during the age of {Modern Thaumatology} and improved upon using various technologies such as neural extension and cognition improvement.
-==RITUAL
-A ritual is a process in which spells are composed and cast. This specific outlined process details a technologically assisted ritual, the most common method in the year 2570.
-=Acquisition
-A thaumaturgist mentally designates a system in a specific location. This system is where the psions are going to impact the infons.
-=Calculation
-An AI scans the system and makes an approximation as to the number of particles present in it. Calculations are then made to determine the present physical properties of each particle and what they will be after the spell has affected them. Once the property solutions are calculated, the AI will send them to the neural interface of the thaumaturgist.
-=Translation
-The neural interface accepts and translates the property solution into information that the thaumaturgist's brain will understand.
-=Psion Generation
-The translated property solutions are interpreted by the brain and translated into psions.
-=Spell Collection
-Mana and psions are pooled into an information and energy package, officially becoming a spell.
-=Spell Delivery
-The spell is either sent unassisted or with an emitter towards the desired target. Due to the lightspeed nature of psions and mana, as well as them exclusively interacting with conceptual particles, a spell-making impact with its target seems to be a spontaneous phenomenon to the average onlooker. For example, a thaumaturgist making use of a fire spell might launch the spell from their wand, but instead of leaving a visible flaming trail, it simply appears at the target at the speed of light.
-==SPELL COMPENDIUM
-=Fusion Point
-Fusion Point is a spell that compresses atomic nuclei in a specific area into a much smaller area, forcing fusion reactions to occur. This spell is used widely in both combat as well as energy production.
-=Atmospheric Repulsion
-Atmospheric Repulsion is a spell that actively pushes air away from the emitter. This is especially useful in both increasing the aerodynamicness of atmospheric craft and acting as a form of propulsion.
-=Material Compression
-Material Compression is a spell that increases the density of particles within a target system. This can be used to increase the sharpness of a blade, reinforce physical armor, or even synthesize hyperelements.
-=Ionize
-Ionize is a spell that converts particles in a system into a plasma, which could be turned into a weapon or as a form of energy shielding.
-=Hardlight
-Hardlight is a spell that forces photons in a given system to slow down to a crawl, which forces them to gain mass. This creates a form of solid light, which is incredibly useful in fields such as quantum computing, superconductor development, and lightspeed weapons development.
-=Psionic Interference
-Psionic Interference is a spell that utilizes a caster’s own psions to interfere with another caster’s psions. This could be used in surveillance to detect when spells are being cast as well as used to interfere with spells in a specific area, creating a form of area denial.
-==SCALING
-Most casting methods can be divided into two categories: static and scalar. While static spells are able to bypass the computation phase of casting, they aren’t adaptive as they produce almost the same phenomenon every time they are casted. However, they can be chained and combined to produce varied effects in a small amount of time. Meanwhile, scalar spells utilize the computation phase to create a spell uniquely suited to a certain situation, meaning lower rates of casting but higher degrees of potency."""
+article = """===HUMAN THAUMATOLOGY
+Ever since humankind's inception, the arcane arts have shifted and twisted the tides of history. From the magus cabals and alchemists of yore to the construct mages and thaumatologists of the modern day, {thaumaturgy} has played a pivotal role in humanity’s development as a species.
+==ANCIENT THAUMATOLOGY
+Ancient Thaumatology refers to the method of archaic and inefficient {spellcasting} utilized by mages of old. This was due to a combination of lack of understanding and technological assistants. During this time, cabals of mages and alchemists, usually elderly, would gather and chant a series of words officially now called Verses. These were psychological tools that helped multiple casters envision the same, or close to the same phenomena. By combining their mana and psions, they would will this envisioned phenomena into reality. Usually, this phenomenon is now considered to be of the tame variety, perhaps the lighting of a candle or the subtle increase of fog in an area. However, this art was inconsistent and rudimentary, with mana and psions being regarded as inefficient from a modern standpoint. While Ancient Thaumatology was devastating on battlefields of swords and shields, Division historians surmise that the increase of wars fought with black powder brought this period to an end.
+==MODERN THAUMATOLOGY
+The time at which the age of Ancient Thaumatology ended and Modern Thaumatology began is a highly debated topic amongst scholars. However, there are some standings that have more support than most. Many say that the discovery of conceptual particles marked the beginning of Modern Thaumatology. Others say it was the Lifted Veil Event, where thaumaturgical information was disseminated to the public for the first time in the mid-twentieth century. Some go even further, purporting that the beginning of the Thaumaturgical Revolution was the catalyst for Modern Thaumatology.
+==DISCOVERY OF CONCEPTUAL PARTICLES
+While the discovery of conceptual particles was a convergent event amongst many paraphysical institutes, two scientists are credited with formulating the complete theory of paraphysics. The first was Johannes Siddhar of the United States’ Bureau of Paraphysics, who devised a paper titled “Esoteric Energies and Their Properties” that detailed the energy now known as mana. The second was Solana Igarashi’s “Theory of Conceptual Particle Interactions,” written in the USSR’s State Research Center of Koltsovo, which discussed the existence of infons and psions. They had the two pieces of the puzzle, but due to their organizations being hostile and separate, further developments were unable to be pursued. However, after the Terran Accords were signed and their respective organizations were merged, the two scientists were finally able to collaborate, construct, and complete the Theory of Thaumatology, establishing the foundation for Modern Thaumatology.
+==LIFTED VEIL EVENT
+The Lifted Veil Event spelled the end for most of the Paraphysics Division’s secrecy. The knowledge of the arcane arts and Modern Thaumatology were disseminated throughout the Human Domain. It was a landmark day for humanity, with thaumatology, in other words, magic, being regarded as real by mainstream scientists and physicists. Historians scrambled to rewrite their thesis while conspiracy theorists went crazy from the idea that some of their ideas were actually true. Prevailing questions humankind had about its own history were finally answered, and the science of thaumatology became a respected study in many schools.
+==THAUMATURGICAL REVOLUTION
+While the discovery of conceptual particles and esoteric energies were incredibly impactful for human society and understanding of history, the phenomena on their own had little to no application at the time. However, a series of keystone technological developments allowed humanity to finally harness these new forces to their full potential.
+=Life Extension
+As the age of a person increases, so does their mana production rate. Due to this, many thaumaturgists of the ancient world were quite old, having spent the majority of their lives honing this art. However, the invention of life extension technology allowed any person, provided they live long enough, to eventually gain the mana pool necessary to pull off spells.
+=Neural Networking and Cognition Improvement
+The invention of brain-computer interfaces and neural computers afforded modified humans a much higher degree of cognition and psion output compared to their unmodified counterparts. It also allowed humans to meld their minds with each other, allowing for more effective group casting. This was instrumental in placing thaumaturgy in the hands of the average citizen.
+=Emitter Development
+The development of spellcasting devices allowed for more effective spell composition and delivery. The creation of technologies such as wands and somatic threading enabled anyone to wield thaumaturgy with efficiency as well as integrating thaumaturgy in already existing technologies."""
 
 # this is the file the html gets saved in, make sure to include
 # the .html extension
-filename = "humanspells-lore.html"
+filename = "humanthaumatology-lore.html"
 
 # ignore this stuff
 article = article.replace('{', "<a class='infolink' href='(LINKGOESHERE)'>")
