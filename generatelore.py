d314532b9494e441f71cfc7cfecc5939700f15f3
Maxwell Zen <maxwell.zen@gmail.com>
2021-08-31 23:15:25 -0400

    Added Ikolae lore

--- generatelore.py
+++ generatelore.py
@@ -2,27 +2,27 @@
 
 # This will be displayed as the title of the tab, ex "Aeternum"
 # will display the title "Aeternum - Project Anima" on the tab
-tabtitle = "Human Thaumatology"
+tabtitle = "Ikolae"
 
-keywords = "​​​​​​arcane arts, magus cabals, alchemists, mages, thaumatologists, thaumaturgy, Ancient Thaumatology, Verses, Division historians, black powder, Modern Thaumatology, conceptual particles, Lifted Veil Event, Thaumaturgical Revolution, Johannes Siddhar, United States’ Bureau of Paraphysics, Esoteric Energies and Their Properies, mana, Solana Igarashi, Theory of Conceptual Particle Interactions, USSR’s State Research Center of Koltsovo, infons, psions, Terran Accords, Theory of Thaumatology, Paraphysics Division, Human Domain, magic, conceptual particles, esoteric energies, life extension, mana production rate, mana pool, neural networking, cognition improvement, brain-computer interfaces, group casting, emitter, wands, somatic threading, spell composition"
+keywords = "​​​​​​custodian races, Elder Races, War of Dreams, sophonthood, Ikolae (the Enlightened Minds), Outlanders, Galactic Commonwealth, Kelleus, spaceborne constructs, orbs, combat frames, central minds, mining frames, void drives, drone complement, maintenance frames, Proto-Ikolae, First Awakening, wormhole gates, Second Awakening, civilizations, collectives, hive-mind, Delegate Collective, Builder Collectives, Caretakers, Weaponsmiths, Mechanic, Guardian Collective, Striker Collective, Miners, voidjumping, centrums, cooling apparatus, Kelleus mainframe"
 
 # This is the information that will appear in the lil box on
 # the right, the order the entries appear in the box is:
 # 1 4
 # 2 5
 # 3 6
-infoheadings = ["CATEGORY", "LOCATION", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"]
-infoentries = ["History", "Human Domain", "Thaumatology", "Human", "Devin Deng", "September 1st, 2021"]
+infoheadings = ["CATEGORY", "DOMAIN", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"]
+infoentries = ["Factions", "Ikolae Domain", "Species", "Ikolae", "Devin Deng", "September 1st, 2021"]
 
 # this is the address of the image that appears in the lil
 # box on the right, make sure it's relative to the html file
 # (probably something like resources/image.jpg)
-infoimagesource = "resources/Human Thaumatology Icon.svg"
+infoimagesource = "resources/Ikolae Icon.svg"
 
 # if the article has links, put the addresses for those links
 # here. They must be listed in the same order as they appear
 # in the article. You can omit the .html extension
-articlelinks = ['paraphysics-lore#thaumatology', 'humanspells-lore']
+articlelinks = ['galacticcommonwealth-lore', 'fasterthanlight-lore#wormhole']
 
 # this is the hardest part so read it carefully, look at the
 # example for reference, make sure text is surrounded by triple quotes.
@@ -49,28 +49,45 @@ articlelinks = ['paraphysics-lore#thaumatology', 'humanspells-lore']
 # 6 - any line that doesn't start with equal signs is interpreted
 #     as a regular paragraph
 
-article = """===HUMAN THAUMATOLOGY
-Ever since humankind's inception, the arcane arts have shifted and twisted the tides of history. From the magus cabals and alchemists of yore to the construct mages and thaumatologists of the modern day, {thaumaturgy} has played a pivotal role in humanity’s development as a species.
-==ANCIENT THAUMATOLOGY
-Ancient Thaumatology refers to the method of archaic and inefficient {spellcasting} utilized by mages of old. This was due to a combination of lack of understanding and technological assistants. During this time, cabals of mages and alchemists, usually elderly, would gather and chant a series of words officially now called Verses. These were psychological tools that helped multiple casters envision the same, or close to the same phenomena. By combining their mana and psions, they would will this envisioned phenomena into reality. Usually, this phenomenon is now considered to be of the tame variety, perhaps the lighting of a candle or the subtle increase of fog in an area. However, this art was inconsistent and rudimentary, with mana and psions being regarded as inefficient from a modern standpoint. While Ancient Thaumatology was devastating on battlefields of swords and shields, Division historians surmise that the increase of wars fought with black powder brought this period to an end.
-==MODERN THAUMATOLOGY
-The time at which the age of Ancient Thaumatology ended and Modern Thaumatology began is a highly debated topic amongst scholars. However, there are some standings that have more support than most. Many say that the discovery of conceptual particles marked the beginning of Modern Thaumatology. Others say it was the Lifted Veil Event, where thaumaturgical information was disseminated to the public for the first time in the mid-twentieth century. Some go even further, purporting that the beginning of the Thaumaturgical Revolution was the catalyst for Modern Thaumatology.
-==DISCOVERY OF CONCEPTUAL PARTICLES
-While the discovery of conceptual particles was a convergent event amongst many paraphysical institutes, two scientists are credited with formulating the complete theory of paraphysics. The first was Johannes Siddhar of the United States’ Bureau of Paraphysics, who devised a paper titled “Esoteric Energies and Their Properties” that detailed the energy now known as mana. The second was Solana Igarashi’s “Theory of Conceptual Particle Interactions,” written in the USSR’s State Research Center of Koltsovo, which discussed the existence of infons and psions. They had the two pieces of the puzzle, but due to their organizations being hostile and separate, further developments were unable to be pursued. However, after the Terran Accords were signed and their respective organizations were merged, the two scientists were finally able to collaborate, construct, and complete the Theory of Thaumatology, establishing the foundation for Modern Thaumatology.
-==LIFTED VEIL EVENT
-The Lifted Veil Event spelled the end for most of the Paraphysics Division’s secrecy. The knowledge of the arcane arts and Modern Thaumatology were disseminated throughout the Human Domain. It was a landmark day for humanity, with thaumatology, in other words, magic, being regarded as real by mainstream scientists and physicists. Historians scrambled to rewrite their thesis while conspiracy theorists went crazy from the idea that some of their ideas were actually true. Prevailing questions humankind had about its own history were finally answered, and the science of thaumatology became a respected study in many schools.
-==THAUMATURGICAL REVOLUTION
-While the discovery of conceptual particles and esoteric energies were incredibly impactful for human society and understanding of history, the phenomena on their own had little to no application at the time. However, a series of keystone technological developments allowed humanity to finally harness these new forces to their full potential.
-=Life Extension
-As the age of a person increases, so does their mana production rate. Due to this, many thaumaturgists of the ancient world were quite old, having spent the majority of their lives honing this art. However, the invention of life extension technology allowed any person, provided they live long enough, to eventually gain the mana pool necessary to pull off spells.
-=Neural Networking and Cognition Improvement
-The invention of brain-computer interfaces and neural computers afforded modified humans a much higher degree of cognition and psion output compared to their unmodified counterparts. It also allowed humans to meld their minds with each other, allowing for more effective group casting. This was instrumental in placing thaumaturgy in the hands of the average citizen.
-=Emitter Development
-The development of spellcasting devices allowed for more effective spell composition and delivery. The creation of technologies such as wands and somatic threading enabled anyone to wield thaumaturgy with efficiency as well as integrating thaumaturgy in already existing technologies."""
+article = """===IKOLAE
+The creation of custodian races and constructs to maintain structures and systems was not uncommon amongst the Elder Races. However, one of the few that have survived the War of Dreams and achieved sophonthood are the Ikolae, the Enlightened Minds. They and their brethren compose a group of nations referred to as the Outlanders by the {Galactic Commonwealth}.
+==CONSTRUCTION AND CAPABILITIES
+The Ikolae of Kelleus are spaceborne constructs of purely mechanical nature. They possess the capability to move through a vacuum and their only need for survival is the spatial energy given off by radiators. Their consciousness rests in an orb that serves as the central processing unit. These orbs are placed into frames that serve a variety of functions, such as mining frames and maintenance frames. This allows the same orb to be placed in a variety of constructs.
+=Orb
+Most orbs possess a rudimentary form of movement in the form of thrusters. This allows them to switch bodies without external aid and retreat if necessary.
+=Combat Frames
+Combat frames are diverse, with units ranging from conventional infantry to defense batteries to starships. Combat frames primarily utilize laser and plasma weaponry and are highly effective, being able to perfectly coordinate with each other through central minds.
+=Mining Frames
+Mining frames are starships equipped with void drives to allow for faster than light travel to the inner galaxy. They also possess a drone complement to aid in the collection of raw materials.
+=Maintenance Frames
+Maintenance frames are equipped with tools such as cutting lasers and power jumpers to maintain the various radiators and manufactorums of the Ikolae.
+==HISTORY
+=Conception
+The death of the Elder Races meant that much of their technology would be destroyed. When this occurred, a kill switch was sent to various facilities to shut them down. However, on Kelleus, this kill switch did not function properly. It split the central AI’s consciousness into various bodies, wiping its memories in the process and birthing the Proto-Ikolae. However, shortly after this occurred, Kelleus lost its power and {wormhole} capabilities, shutting the facility down for a long time. 
+=First Awakening
+Tens of thousands of years later, Kelleus suddenly received a spark of power from an unknown source. The wormhole gates opened and for the first time in millennia, power began flowing throughout the facility, and in turn, to the Proto-Ikolae. Each Proto-Ikolae, while sentient, possessed only the routines and rituals it needed to complete its former tasks.
+=Second Awakening
+For thousands of years the Ikolae communicated with each other mentally, exchanging and downloading information. Eventually, more complex consciousnesses emerged, creating the first sophont Ikolae. These Ikolae formed collectives based on their primary function, and eventually these merged into greater collectives, leading to the establishment of the first civilizations.
+==SOCIETY
+While their memories of the bygone age were wiped, their primary protocols and instincts still remained. Due to this, Ikolae society is divided into various major collectives, which are further divided into minor collectives based on their function. These collectives form a hive-mind, where while each Ikolae possesses their own form of free will, they can actively exchange information with their brethren. New collectives can also be formed to fulfill functions that weren’t previously needed, such as the Delegate Collective.
+=Collectives
+_Builders
+The Builder Collectives are the most respected and numerous of Ikolae. They are responsible for building and maintaining the various environments and constructs of the Ikolae. Some of the major collectives within the Builder Collective include the Caretakers, the Weaponsmiths, and the Mechanics.
+_Warriors
+Warriors within Ikolae society are generally of the Guardian Collective, but as the Ikolae began to expand into the galaxy, they found many hostile fauna and civilizations. Thus, the Striker Collective was created to facilitate the use of preemptive strikes. This was a sharp change compared to the Guardian Collective, whose only functions were to protect the territories and structures of the Ikolae.
+_Miners
+Miners are mobile platforms that acquire and transport various resources to the foundries of the Ikolae. Due to them being deep within the Outlands, they utilize a form of voidjumping to enter the Milky Way and extract resources.
+_Delegate Collective
+The original responsibilities of the Ikolae, established by their creators, did not include forming diplomatic ties with other nations. As a result, when the Ikolae began encountering other sophont species, the collectives were initially at a loss for action. After a long period of discourse and a few wars of wills, they decided that a new collective would be established to form non-violent relationships with these foreign nations. It was due to this the Delegate Collective was created, each member purposely built to accommodate a specific race or culture.
+==CULTURE
+Ikolae culture is highly pragmatic yet peaceful. Physical conflicts between Ikolae rarely occur due to such actions being
+viewed as a waste of resources. Instead, collectives of Ikolae will engage in wars of wills, where the different sides will strain to mentally dominate each other. While possessing forms of individuality, the Ikolae do not engage in actions such as lying and withholding of information, instead seeking to share all information to better Ikolae society.
+==LOCATIONS
+Even the most advanced civilizations cannot circumvent the laws of thermodynamics. Ships and other spaceborne constructs are left in the vacuum of space, a place of perfect insulation. Getting rid of waste heat through large radiators becomes impractical for mobile vessels such as starships when energy generation scales upwards. One of the methods devised to address this issue was the invention of cooling worlds, megastructure scale heatsinks connected to starships via wormholes. Energy is fed through these wormholes and distributed throughout the structures, dissipating it as radiation. One of these locations is Kelleus, the home of the first Ikolae. Kelleus is a radiator-type megastructure that is composed of modular units known as centrums. Centrums usually consist of a wormhole gate and a cooling apparatus. However, additional sections such as foundries and defense batteries can also be produced and integrated into the greater Kelleus mainframe. """
 
 # this is the file the html gets saved in, make sure to include
 # the .html extension
-filename = "humanthaumatology-lore.html"
+filename = "ikolae-lore.html"
 
 # ignore this stuff
 article = article.replace('{', "<a class='infolink' href='(LINKGOESHERE)'>")
