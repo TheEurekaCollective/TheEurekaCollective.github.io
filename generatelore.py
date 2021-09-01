5cf4a5def74d354714164b00ebb452d03c6d0689
Maxwell Zen <maxwell.zen@gmail.com>
2021-09-01 00:03:41 -0400

    added Kalatari lore

--- generatelore.py
+++ generatelore.py
@@ -2,9 +2,9 @@
 
 # This will be displayed as the title of the tab, ex "Aeternum"
 # will display the title "Aeternum - Project Anima" on the tab
-tabtitle = "Operation Helios"
+tabtitle = "Kalatari"
 
-keywords = "​​​​​​Helios Class, Appalachia Rejectionist Enclaves, USNA, Seekers, Intendant Eureka, Stealth Station Tarnhelm, HALO-Assisted Drop Pods, subsonic flight, Scramblers, Huginn reconnaissance drones, aerial hypnotics drone, amnestic application, Cataphract Dropships, Hard-Body Combat Androids, Scrappers, Programmers, non-combat drones, Stasis Containment Unit, Hermetic Biovault, Gotfir, House Faber"
+keywords = "​​​​​​Kalatari, animium-based life, sophont, reptilian, Skago, Skaga, caste, Revelator, Concordat, Arhima, Thosik, Galactic Commonwealth, warriors, fabricators, claytronic, animium, polymorphism, Andukalatar, Konokalatar, Wuxak, Kalatar, tribes, cataclysms, Kalathron, Husk Crusades (Wars of Unification), Wellspring, Prinor Empire, Mana Reactor, Animium Heart, Xanthus War, Xanthus Thorns, the Fracturing, Fourth Animium Heart, Animium Wars, steel-flesh, faster-than-light weapon, Era of Reconciliation, ecclesiastical aristocracy, ecclesio-aristocrats, Rulers, Sovereigns, Sophists, Trials of the Sun, Zuga, Thelmik, Prinor, Lok Windha, (Beasthunter’s Manual), Way of the Firedrake, Way of the Lanceworm, slingfish, magma sculpting, anti-armor weapons, greatbows, Juknoza, bone sculpting, Kalatashue, Retainers, optical computing device, Xanthus Boons, Silver Tower, Perseus Arm, City of Three Peoples, Luminous Falls, Battle Braid of Irithraad, Irithraad (the Standing Shield), Adulkar’s Greatbow, cyclopean firedrake, origin-grade animium, Queenslayer Adulkar"
 
 # This is the information that will appear in the lil box on
 # the right, the order the entries appear in the box is:
@@ -12,17 +12,17 @@ keywords = "​​​​​​Helios Class, Appalachia Rejectionist Enclaves, US
 # 2 5
 # 3 6
 infoheadings = ["CATEGORY", "DOMAIN", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"]
-infoentries = ["Stories", "Human Domain", "Mission Log", "Human", "Devin Deng", "September 1st, 2021"]
+infoentries = ["Factions", "Kalatari Domain", "Species", "Kalatari", "Devin Deng", "September 1st, 2021"]
 
 # this is the address of the image that appears in the lil
 # box on the right, make sure it's relative to the html file
 # (probably something like resources/image.jpg)
-infoimagesource = "resources/Operation Helios Icon.svg"
+infoimagesource = "resources/Kalatari Icon.svg"
 
 # if the article has links, put the addresses for those links
 # here. They must be listed in the same order as they appear
 # in the article. You can omit the .html extension
-articlelinks = ['combatmage-lore']
+articlelinks = ['galacticcommonwealth-lore', 'silvertower-lore']
 
 # this is the hardest part so read it carefully, look at the
 # example for reference, make sure text is surrounded by triple quotes.
@@ -49,54 +49,114 @@ articlelinks = ['combatmage-lore']
 # 6 - any line that doesn't start with equal signs is interpreted
 #     as a regular paragraph
 
-article = """===OPERATION HELIOS
-Mission Log Start
-Terran Time: 22:30
-Terran Date: 24-11-2570
-Subject: Candidate Extraction
-Codename: Operation Helios
-00:00: Helios Class Candidate detected and confirmed in the Appalachia Rejectionist Enclaves of the USNA.
-02:00: 7 {Seekers} and the local drone contingent coordinated by Intendant Eureka were deployed from Stealth Station Tarnhelm.
-04:00: Seekers made landfall through the use of HALO-Assisted Drop Pods twelve kilometers from the target destination. On subsonic flight, they approached the target destination.
-4:30:  Seekers reached the Rejectionist Compound. Sensor Scramblers were activated and Huginn reconnaissance drones were able to obtain a lock on the Host and Candidate. 4 Seekers were deployed to establish a perimeter while 3 Seekers were sent to retrieve the Targets.
-05:10: Entire compound successfully put to sleep via aerial hypnotics drone.
-05:14: Host and Candidate were apprehended. In-situ Host-to-Container surgery and amnestic application initiated.
-06:57: Detection Sector Z breached. 23 Cataphract Dropships and 7300 drones of unknown origin descended upon the compound. Seekers and friendly drones were able to destroy 21 dropships and 7043 drones before they made landfall. The two dropships that remained were able to deploy 170 Hard-Body Combat Androids.
-08:17: 2 dropships, 170 HBCAs and 257 unfriendly drones successfully eliminated. Total casualties were 562 friendly drones, 8 civilians, and 0 Seekers.
-10:42: Candidate successfully extracted and Seekers exited the compound. Scrappers and Programmers deployed. Non-combat drones recalled to local retention facility. Non-combat drones memory wiped.
-21:00: Seekers and Candidate returned to Tarnhelm via Imran Stealth Dropship. Seekers placed to sleep. Candidate transferred to Stasis Containment Unit. Unit placed in Hermetic Biovault 3.
-65:26: Scrappers’ and Programmers’ tasks completed. Returned to a local retention facility. Memories wiped.
-Mission Log Finish
-Administrator: Intendant Kanté
-Addendum: Upon further inspection, the Cataphract Dropships, drones, and HBCAs seemed to originate from Gotfir, a proxy of House Faber. """
+article = """===KALATARI
+The Kalatari are a race of animium-based life evolved from the sophont reptilian Skago, who in turn evolved from the non-sophont Skaga. Their entire civilization hinges on a caste system controlled by the clans of Revelators. The Kalatari are a component of the Concordat, composed of the Kalatari, Arhima, and Thosik. This collective is not a component of the {Galactic Commonwealth} since it prefers to maintain its autonomy.
+==EVOLUTION & BIOLOGY
+=Heritage
+The Kalatari originated as the reptilian Skago, who evolved from the non-sophont Skaga.
+=Physiology
+Their bodies are relatively humanoid in shape with two arms, two legs, and a defined head. Their arms consist of three segments and have three fingers. Their legs are digitigrade and end in three thick toes that serve as the underfoot. Their head contains six eyes and a horizontally-opening mouth with a pharyngeal jaw. The holes underneath their jaws serve as respiratory systems. To survive in the brutal environment of their home planet, they developed robust respiratory systems and thick black hides. They are somewhat tall, with warriors and fabricators being two and a half to three meters in height. Bone plates would extend over their body for protection. After the claytronic, known as animium, was integrated into their biology, their bones and plating would be converted into animium. On average, the females are much larger than the males.
+=Reproduction
+Males possess birthing pouches that females deposit their eggs into. Immediately following birth, the young possess very little physical protection due to their lack of outer plating. This results in them needing to be tended to for the first few years of their life.
+=Polymorphism
+Due to a combination of environmental pressures and social engineering, the castes of the Kalatari exhibit strong mental and physiological differences.
+==HISTORY
+Kalatari history could be divided into three ages. They are Andukalatar, Konokalatar, and Wuxak. Andukalatar is the era before Kalatar’s arrival, Konokalatar is the age after Kalatar’s arrival, and the Wuxak is the age of the Concordat.
+=Andukalatar
+The Skago were split between thousands of tribes of hunter-gatherers. While once interconnected, a series of cataclysms split the lands of Kalathron, isolating many of the Skago tribes. This was an extremely long period, as the cataclysms had destroyed a large amount of the Skago population. It took them some time to regrow, but over the long isolation between different environments, the Skago evolved into different forms. This became the basis for the Kalatari caste system.
+=Konokalatar
+_Husk Crusades & Wars of Unification
+While the main portion of Kalatar landed within the Wellspring, several shards of Kalatar had crashed into other locations on Kalathron. Some pieces had even spent some time in orbit, allowing pieces of the Kalatar to land far away from the Wellspring. In an effort to rebuild the body of their god, the Prinor Empire of the Wellspring, Chosen of the Revelator, engaged in a series of crusades. Over the next thousand years, Kalatar was reconstructed to nearly 93 percent of its original body. This brought back many of Kalatar’s subsystems, such as the Mana Reactors and Animium Hearts. This was also a period of assimilation, when the Prinor people subjugated or made alliances with the other tribes. The tribes that settled peacefully with the Prinor were able to form their own houses and clans within the Empire. Due to this, the Husk Crusades were also referred to as the Wars of Unification.
+_Xanthus War
+When the Xanthus Thorns were cleaved, its pieces were split amongst the three main clans of sovereigns. When the Prinor Empire was thrown into disarray, this event became known as the Fracturing. This was due to the secret creation of the Fourth Animium Heart. This freed them from the control of the Ruler and allowed them to overthrow it.
+_The Animium Wars
+When the Kalatari took to the stars, they found that they were not the only ones who had been touched by Kalatar. Of the seven planets that Kalatar had experimented with its animium strain, three had survived the process and produced sophonts. They were the Arhima and Thosik species. The discovery that these other races did not worship their lord, even after being gifted with steel-flesh, was heresy. Thus began the Animium Wars. The use of a faster-than-light weapon that destroyed Kalathron ended the Animium Wars.
+=Wuxak
+The Era of Reconciliation began when the three races of animium-based life signed a peace treaty and formed the Concordat. The Concordat has existed for hundreds of years and is the current era for the Kalatari.
+==SOCIETY
+Kalatari Society is divided into a form of a caste system that is headed by an ecclesiastical aristocracy. Members are born into their respective castes, and due to distinct social and morphological differences, there is little to no form of social mobility within Kalatari society. While each caste is generally considered equal, the interdependence on each caste’s services and products causes their standings to fluctuate based on which families hold the throne.
+=Ecclesio-Aristocrats
+The ecclesiastical aristocracy comprises the families of revelators. They possess the longest lives and are bred to be bearers of the Xanthus Thorns. These families originated from the tribes that yielded willingly during the Unification Wars.
+_Rulers
+Before the Fracturing, the entirety of Kalatari society was ruled by a single Revelator. After the Kalatari Civil War and the splitting of the Xanthus Thorns, this power was split between three, less powerful Revelators. Pre-Fracture, Revelators could live for thousands of years; the longest being nearly 2,000 years. Post-Fracture, Revelators could now only live for a few hundred years.
+_Sovereigns
+The Sovereigns are three families that hold the pieces of the fractured Xanthus Thorns.
+=Sophists
+The sophist caste is composed of merchants, diplomats, and inventors. They are physically weak, but possess a long lifespan. Ecclesio-aristocrats frequently interact with the Sophists, much more than the interaction between the other castes. Sophists have a low reproductive rate and are clad in the least plating.
+=Warriors
+Warriors serve as the combat faction of the Kalatari people. They are born with high degrees of endurance and strength, but only a moderate lifespan. They have high reproductive rates and possess the most natural plating out of all the castes.
+=Fabricators
+Fabricators are the primary laborers of Kalatari society as they are strong yet skilled with their hands. They have a high reproductive rate to compensate for their short lives.
+=Trials of the Sun
+Kalatari who have committed crimes can be exiled to the surface of Kalathron without protection from the hazardous environmental conditions, a sentence known as the Trials of the Sun. If they survive for a single year, they are allowed back into Kalatari society. However, their burns would forever mark them as a post-exile.
+==CULTURE
+Much of Kalatari culture stems from their tribal roots, which can be seen in tribes, such as the Zuga and the Thelmik, that submitted to the Prinor Empire.
+=Prinor
+The Prinor hail from a proud warrior tradition of beast hunters. They would hunt these great beasts and turn their bones into disks. Short poems speaking of the event would be composed and inlaid into the disks with precious metals, which would then be placed on their battle braid. They are also the proprietors of the Lok Windha, otherwise known as the Beasthunter’s Manual. They utilize a variety of combat techniques named after various animals, such as the Way of the Firedrake and the Way of the Lanceworm.
+=Zuga
+The Zuga were nomadic merchants that connected the tribes together via the use of underground waterways. As a result, they were expert divers and were able to domesticate slingfish, massive creatures akin to whales that would help them carry cargo between cities.
+=Thelmik
+The Thelmik were masons that perfected the art of magma sculpting. They would guide lava flows into various shapes and rapidly cool them, forging obsidian blades and castles of igneous rock.
+=Warfare
+Ancient Kalatari warfare consisted of both underground and surface battles. While underground, close-quarters engagements were common, while surface battles were usually fought at night.
+_Anti-Armor Weapons
+Due to the heavily-armored nature of most Kalatari, warriors would make extensive use of bludgeoning weapons such as war hammers and war spikes as well as precision stabbing weapons such as estocs.
+_Greatbows
+The Kalatari warriors’ impressive strength and constitution allow them to wield bow variants known as greatbows. These were bows with draw weights ranging in the hundreds of kilograms, and fired arrows the size of javelins. Precision greatbows and warhammers were the preferred weapons of many Kalatari hunters.
+=Juknoza
+Juknoza is a festival where Kalatari and other creatures would go up to the surface during the daytime while a solar eclipse takes place. This eclipse would be present for weeks on end and happens once every year.
+=Bone Sculpting
+The plating that adorns Kalatari bodies lacks pain receptors, allowing Kalatari to modify them to their liking. Their growth could be influenced into unnatural shapes, such as the shoulder plating Kalatari warriors grow to prevent melee weaponry from deflecting into their exposed necks. Wealthy Kalatari can also afford to inlay their plating with precious resources such as the bones of the creatures they have slain.
+==RELIGION
+The state religion of the Kalatari is that of Kalatashue, literally translating to “Worship of Kalatar”. While there is an official form of worship within Kalatashue, many Kalatari worship Kalatar differently by incorporating their own cultural values.
+=Ranks
+The highest rank of the ecclesiarchy and the head of state is Revelator. Below them are disciples known as Retainers. They handle the various administrative and maintenance tasks such as tending to the body of Kalatar.
+=Religious Artifacts
+_The Xanthus Thorns
+The Xanthus Thorns is theorized to be an optical computing device that can interface with biological synapses. It gifted its bearer several boons (Xanthus Boons), including long life, access to the memories of past Revelators, control over some of Kalatar’s systems, as well as control over animium. The prayer position utilized by the followers of Kalatashue was inspired by the shape of the Thorns.
+==LOCATIONS
+=Kalathron
+The world of Kalathron revolved around a cruel sun. Great waves of plasma would sweep across the planet, cursing those it came in contact with. However, life found a way to survive, not on the surface, but within deep caverns. From above, stone skies shielded life from the scorching rays, allowing only what was necessary to pass through. From below, volcanic vents would feed the various flora and fauna, allowing life to eventually advance to that of a sophont species, a reptilian race known as the Skago. This planet was eventually destroyed during the Animium Wars.
+={Silver Tower}
+Near the edge of the Perseus Arm, within the City of Three Peoples, lies an ark-turned-temple, a statement of Kalatari faith. It holds the Wellspring, the Luminous Falls, but most importantly, it holds the Kalatar’s Body. It is their Mecca, their Jerusalem, their Vatican. It is the Silver Tower. It serves as a hub for all of its worshippers. Billions of Kalatari come to the Silver Tower every day to pray and worship their creator. However, most worshippers live within the orbital rings and platforms that encircle the tower. From Kalatar’s body spouts animated metal, which is to be harvested and shipped throughout the Kalatari Domain.
+=Cultural Artifacts
+_Battle Braid of Irithraad
+The greatest warrior to ever live was Irithraad, the Standing Shield. This warrior of gargantuan proportions bested many great foes and beasts, accumulating a battle braid with nearly 1,000 disks.
+_Adulkar’s Greatbow
+Adulkar’s Greatbow is a ranged weapon made from the bone of a cyclopean firedrake and enhanced with origin-grade animium. Its construction took several decades with the combined efforts of dozens of bowyers and breeders. The resulting bow was wielded by Queenslayer Adulkar during the Wars of Unification, allowing her to snipe many enemy leaders.
+==ECONOMY
+Origin Grade Animium: Origin-grade animium is animium produced by the original three Animium Hearts of Kalatar. These are the purest and most effective strain of animium. It degrades less quickly and is used to construct the finest goods and supplements.
+==NOTES
+Kalatar is a shortened name that roughly translates to “Fallen Star”. """
 
 # this is the file the html gets saved in, make sure to include
 # the .html extension
-filename = "operationhelios-lore.html"
+filename = "kalatari-lore.html"
 
 # ignore this stuff
-article = article.replace('{', "<a class='infolink' href='(LINKGOESHERE)'>")
-article = article.replace('}', "</a>")
-for link in articlelinks:
-	article = article.replace('(LINKGOESHERE)', link, 1)
 
 htmlcode = ""
 lines = article.split('\n')
 for line in lines:
+	curID = line.lower().replace(' ', '-').replace('{', '').replace('}', '').replace('=', '').replace('_', '')
+	for c in "{}=_":
+		if c in curID:
+			curID = curID.replace(c, '')
 	if len(line)>=3 and line[:3]=='===':
-		curID = line[3:].lower().replace(' ', '-')
 		htmlcode += "\t\t<h1 id='"+curID+"'>" + line[3:] + "</h1>\n\t\t<span class='underline1'></span>\n"
 	elif len(line)>=2 and line[:2]=='==':
-		curID = line[2:].lower().replace(' ', '-')
 		htmlcode += "\t\t<h2 id='"+curID+"'>" + line[2:] + "</h2>\n\t\t<span class='underline2'></span>\n"
 	elif len(line)>=1 and line[:1]=='=':
-		curID = line[1:].lower().replace(' ', '-')
 		htmlcode += "\t\t<h3 id='"+curID+"'>" + line[1:] + "</h3>\n"
 	elif len(line)>=1 and line[:1]=='_':
-		curID = line[1:].lower().replace(' ', '-')
 		htmlcode += "\t\t<h4 id='"+curID+"'>" + line[1:] + "</h4>\n"
 	else:
 		htmlcode += "\t\t<p class='infoparagraph'>" + line + "</p>\n"
+
+htmlcode = htmlcode.replace('{', "<a class='infolink' href='(LINKGOESHERE)'>")
+htmlcode = htmlcode.replace('}', "</a>")
+for link in articlelinks:
+	htmlcode = htmlcode.replace('(LINKGOESHERE)', link, 1)
 
 s = open('loretemplate.txt', 'r').read()
 s = s.replace('(PAGETITLE)', tabtitle)
