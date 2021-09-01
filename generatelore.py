94db6ad755891944d2edc363356741d7ce015128
Maxwell Zen <maxwell.zen@gmail.com>
2021-08-31 20:25:08 -0400

    Added Cosmology lore

--- generatelore.py
+++ generatelore.py
@@ -2,9 +2,9 @@
 
 # This will be displayed as the title of the tab, ex "Aeternum"
 # will display the title "Aeternum - Project Anima" on the tab
-tabtitle = "Combat Mages"
+tabtitle = "Cosmology"
 
-keywords = "​​​​warfare, paraphysics, ultimate soldier, combat mage, mages, Human Domain, Project Nephilim, Experimental Weapons Department, mech suits, Hard Body Infantry units, House Faber, deep archives, Paraphysics Division, Mage Program, Department of War, Mana Production Potential (MPP), Magus Sector, Drone Combat, Aerial strike drones, breaching drones, chained-wand drones, Armament, Spell Rifles, spells, Sidearm, laser pistol, reactor energy, somatic, Melee, Redundant Oscillating Photoblade, vibrating blade, plasma emitter, plasma cutting, sharpening sheath, Sub-Wand, wand, paraphysical machinery, Hand-to-Hand, thaumaturgical, artificial, muscles, mothership, drone complement, orbital strike, Electronics and Sensors, Viruses, Defense, onion model of defense, flight packages, sensor scramblers, electronic warfare systems, decoy drones, dummy drones, shield drones, kinetic weaponry, plasma weaponry, active camouflage systems,Null Psionic Fields,Psionic Fields,psionic interference system,psionic deadlock,Energy Shielding, Nanolaminate Plating, Reinforcement Spells, chassis, Nanite Protocols, nanites, CCC, redundant life support system, mobility package, Reconnaissance, sensor packages, Communications, mentally communication, cognition stack, mage-to-drone communication, Logistics, mule drones, ordnance packages, Gear, biomechanical, Research and Technology Sector, space superiority, faster-than-light weaponry, drone and android warfare, paraphysical elements, breaching pods, personnel launch tubes, Swarm Mages, Stealth Mages, sensor duping technology, Juggernaut Mages, Scout Mages, Seeker Mages, repulsors, arachnid-type Njordian flight pack, Sniping Mages, Zero-G Mages"
+keywords = "​​​​the Outside, It Who Sleeps (the Dreamer), Existences, Material Plane, Conceptual Plane, Void, universe, dimensions, micro-universes, the Abyss, metaphysics, souls, anima, conjecture, apotheons, mana, mass shadows, faster-than-light (FTL), voidjumping"
 
 # This is the information that will appear in the lil box on
 # the right, the order the entries appear in the box is:
@@ -12,17 +12,17 @@ keywords = "​​​​warfare, paraphysics, ultimate soldier, combat mage, mag
 # 2 5
 # 3 6
 infoheadings = ["CATEGORY", "DOMAIN", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"]
-infoentries = ["Characters", "Human Domain", "Construct", "Human", "Devin Deng", "September 1st, 2021"]
+infoentries = ["Subjects", "Existence", "Existence", "Mixed", "Devin Deng", "September 1st, 2021"]
 
 # this is the address of the image that appears in the lil
 # box on the right, make sure it's relative to the html file
 # (probably something like resources/image.jpg)
-infoimagesource = "resources/Combat Mages Icon.svg"
+infoimagesource = "resources/Cosmology Icon.svg"
 
 # if the article has links, put the addresses for those links
 # here. They must be listed in the same order as they appear
 # in the article. You can omit the .html extension
-articlelinks = ['paraphysics-lore', 'paraphysicsdivision-lore', 'magus-lore']
+articlelinks = ['metaphysics-lore', 'fasterthanlight-lore']
 
 # this is the hardest part so read it carefully, look at the
 # example for reference, make sure text is surrounded by triple quotes.
@@ -49,95 +49,22 @@ articlelinks = ['paraphysics-lore', 'paraphysicsdivision-lore', 'magus-lore']
 # 6 - any line that doesn't start with equal signs is interpreted
 #     as a regular paragraph
 
-article = """===COMBAT MAGES
-Thousands of years of warfare and the discovery of {paraphysics} culminated in humanity’s ultimate soldier: the combat mage. Of all the mages within the Human Domain, nearly 60 percent are classified as combat mages.
-==HISTORY
-=Project Nephilim
-Project Nephilim was an undertaking by the Experimental Weapons Department to develop large humanoid mech suits for use in very specialized urban combat scenarios. The project made it past the development phase and a few prototypes were able to see field testing. However, this proved to be a failure when they were outcompeted by the smaller and more mobile Hard Body Infantry units employed by House Faber. As a result, the project was disbanded and the suits were placed in deep archives. However, many of the technologies would be requisitioned by the {Paraphysics Division} for use within the combat branch of the {Mage Program}.
-=Department of War
-The Department of War was established to use mages within the lower Mana Production Potential (MPP) ranges as infantry and special operations units. This was established shortly after the creation of the Magus Sector within the Paraphysics Division in the early 22nd century.
-==OFFENSE
-=Drone Combat
-Depending on the mission, a combat mage can be paired with a drone complement so as to assist mages in battle. Aerial strike drones, breaching drones, and chained-wand drones can be used by a mage for offensive purposes.
-=Armament
-_Spell Rifles
-Spell Rifles are the primary form of armament used by combat mages. They are precision tools dedicated to aiding a mage in focusing their spells and delivering them over long distances towards targets. They allow a mage to convert what would have initially been a random spray of information into a condensed beam. These weapons are recoilless, silent, and hermetically sealed to prevent tampering.
-_Sidearm
-In the event that the spell rifle and its spare parts are exhausted or rendered inoperable, a mage is issued a laser pistol. This pistol runs off of the mages’ reactor energy and can connect to the mage somatically. Shots from these pistols can deal severe damage to light armor. At maximum charge, shots can punch through heavy armor.
-_Melee
-In the occasional situation where a close-quarters combat is deemed necessary, mages are issued a Redundant Oscillating Photoblade. It consists of a vibrating blade and a plasma emitter, allowing for both conventional and plasma cutting. This knife is stored in a sharpening sheath.
-_Sub-Wand
-A mage has a backup wand for utility purposes. This can include interfacing with paraphysical machinery or just as an offensive tool if the spell rifle is rendered unusable.
-_Hand-to-Hand
-If a mage is left drone- and weapon-less, they can use thaumaturgical hand-to-hand combat. A mage's artificial muscles are incredibly strong and are able to crush titanium given some effort. Mages can also download martial arts information from their motherships or drone complement to gain a further edge in combat. Spells can also be deployed to reinforce the body of the mage.
-=Mothership
-In extenuating circumstances, a mage can call for an orbital strike from its mothership. In this case, one of the ship's weapons can be aimed and fired at the mage’s designated location.
-=Electronics and Sensors
-Viruses and other tools may be generated and deployed to disable enemy software and utility systems.
-==DEFENSE
-While a mage is primarily an offensive construct, it has defense systems to improve the survivability of itself and its allies. To better understand this, a list was compiled using the template of the onion model of defense.
-=Avoid Encounter
-Due to the fear of the enemy capturing a live mage, mages are only deployed in situations where victory is guaranteed. Many mages are also equipped with flight packages to avoid confrontation when necessary.
-=Avoid Detection
-Most mages are paired with a combination of stealth systems and stealth drones. Mages can also switch to low power mode, which allows their temperature to blend into that of the surrounding environment to avoid detection.
-=Avoid Acquisition
-When entering combat, a mage and its drone complement will make use of sensor scramblers and electronic warfare systems to avoid being target-locked by the enemy. They can also deploy dummy drones so as to increase the possible targets the enemy needs to engage.
-=Avoid Hit
-While it is very hard for a mage to dodge a spell or a laser, depending on the distance, conventional kinetic and plasma weaponry could be dodged. It and its drone complement can utilize active camouflage systems to avoid conventional visual detection. Shield and decoy drones can also be used to increase the number of layers between a mage and a projectile.
-=Avoid Penetration
-_Null Psionic Fields
-Psionic Fields are an omni-directional psionic interference system that prevents spells from being effective within a specific area. These fields are based on the principle of psionic deadlock. These fields can be manipulated and moved around to afford better protection in certain circumstances.
-_Energy Shielding
-Energy Shielding is a layer of energized particles that disperses energy from a single impact point to throughout the body of the shield. However, sufficient amounts of energy will overload the shield, forcing it to shut down as a fail-safe.
-_Nanolaminate Plating
-The most widely used form of physical plating is nanolaminate plating. These plates are composed of micron-scale layers of varying elements and compounds. These layers are placed on top of each other in different orientations to induce different properties while also accomplishing high density. These layers can be modified to induce different properties such as thermal resistance, thermal dispersion, impact toughness, and malleability.
-_Reinforcement Spells
-A mage can utilize spells to reinforce the material of its chassis and its drone complement for increased material strength.
-_Nanite Protocols
-In the event of a nanite attack, personal nanites can be deployed to combat them. The surface of the mage’s chassis can also be superheated or supercharged to shed off nanites.
-_Shape
-The fields, shields, and plates of a mage are placed and oriented in such a way so as to deflect impacts as well as decrease drag.
-=Avoid Kill
-The CCC is outfitted with redundant life support systems and a small mobility package so as to afford it the ability to escape death or capture. If the chassis of a mage is damaged, nanites could be deployed to reinforce the damaged area temporarily.
-=Avoid Capture
-In the rare instance that a combat mage is rendered offline, its drone complement will converge on the location to defend the mage. In the instance that this fails, and retrieval is deemed impossible, the CCC has a built-in personal explosive that will trigger and destroy everything, including itself, within a 10-meter radius.
-==RECONNAISSANCE
-The majority of combat mages are equipped with both a mechanical and a flight movement system. These systems combined with aerodynamically efficient shields have allowed for rapid coverage of ground. This high mobility combined with an array of advanced sensor packages allows for a mage to acquire large amounts of battlefield data. Dedicated reconnaissance drones and motherships can link with the mage to provide further reconnaissance capabilities.
-==COMMUNICATIONS
-Through encrypted wireless signals, mages can mentally communicate with each other and their drones instantly. A single mage on its own can coordinate nearly 100 drones, but with the assistance of an off-site cognition stack, they can coordinate a theoretically infinite amount of drones. This highly effective mage-to-mage and mage-to-drone communication can allow for highly advanced and precise combat maneuvers. Mages also maintain communication with their mothership to accept further information and orders.
-==LOGISTICS
-Mages are designed to operate for several years without any sort of maintenance. However, when a mage is damaged and needs to be redeployed quickly, it can either repair itself or have a team of repair drones does it. Materials and equipment could be supplied with mule drones and ordnance packages from motherships.
-==GEAR
-A combat mage’s chassis is its body and its method of interaction with the environment. It’s a biomechanical frame composed of the most advanced materials produced by the Paraphysics Division. New versions of chassis are always being generated by the designer AI of the Research and Technology Sector.
-==APPLICATION
-The advent of space superiority and faster-than-light weaponry has made most cases of infantry warfare impractical. The advent of drone and android warfare has rendered even the most skilled human combatants obsolete. However, there are some situations where deploying mages is advantageous.
-=General
-On the extremely rare occasion that a land battle is waged and paraphysical elements are present, mages are deployed to deal with the paraphysical element. While much rarer, they also vastly outcompete almost all other infantry units within the Human Domain. They are most often assisted by a host of drones as well. These include aerial scouts, sniping platforms, and electronic warfare units.
-=Asset Retrieval and Espionage
-When stealth is of the utmost importance, it is best to send in a squad of Stealth Mages.
-=Extermination
-When a location is ravaged by either an artificial or natural creature, and the destruction of the location is deemed unnecessary or counter-productive, mages are deployed.
-=Personnel and VIP Protection
-Important figures have their own secret attachments of mages to protect them. These attachments are not known to the public so as to protect the existence of mages.
-=Boarding Actions
-When invading enemy ships and structures, mages can be placed in breaching pods, which are sent through personnel launch tubes.
-==VARIATIONS
-=Swarm
-Swarm Mages utilize a spheroid form that is covered with nanite nodes. Their main form of movement is flight and their main method of manipulation is using nanites.
-=Stealth
-Stealth Mages are the smallest out of all their counterparts. They utilize an infiltration chassis that utilizes advanced sensor duping technology.
-=Juggernaut
-Juggernaut Mages are mages primarily designed to protect VIPs and critical locations. They are heavily armored and armed. This decreases much of their reaction time and speed but they more than make up for it in terms of sheer firepower.
-=Scout & Seeker
-Scout Mages are designed to be hyper-mobile and agile. While physically weaker than some of their more heavily armored/muscled counterparts, Scout Mages more than make up for it in speed. With the use of repulsors, they can easily run hundreds of kilometers per hour. With an arachnid-type Njordian flight pack, they can easily exceed Mach-3 within the Terran atmosphere. Seeker Mages are very similar but are deployed specifically to extract mage candidates.
-=Sniper
-Sniping Mages are designed to rely primarily on their drone complement for defense, supply, and reconnaissance. They are glass cannons designed to take out small starships and high-priority targets.
-=Zero-G
-While all mages can survive the vacuum of space naturally, a particle suspension field is given to mages specifically designed to operate in zero-gravity environments. This allows mages to utilize energy shielding."""
+article = """===COSMOLOGY
+Within the Outside, there resides a being. It is known by many names, but its most prominent is It Who Sleeps, otherwise referred to as the Dreamer. It is a being that undergoes the same repeated cycle of slumber and wakefulness. By dreaming, it undergoes a process of creating Existences, and by waking, it undergoes a process of destroying them. Countless Existences have been fashioned and dismantled in this manner. Anima is simply one of these myriad Existences, and it shall not be the last.
+==EXISTENCE
+Within all of Existence, there exists three planes of reality. The first is the Material Plane, which is the universe as we know it. The second is the Conceptual Plane, the realm of souls. The last is the Void, the land of in-betweens.
+=Material Plane
+The Material Plane is our physical universe of matter and energy. It is a four-dimensional space composed of three spatial dimensions and one time dimension. This space contains trillions of galaxies and is billions of years old.
+_Abyss
+Residing in the material plane is a series is interlinked micro-universes collectively referred to as the Abyss. These micro-universes are realms dominated by onturgists.
+=Conceptual Plane
+The Conceptual Plane is a sister universe to the Material Plane. It is the plane of {metaphysics}, souls, and anima. Everything that could be categorized ontologically exists in this universe, from the idea of a cup to a quark. However, this is all conjecture as no material being has ever entered this Plane. The only information about the Conceptual Plane has come from the anecdotes of various anima and apotheons.
+=Void
+The Void is a higher-dimensional space that resides in the space between the Material Plane and the Conceptual Plane. As the two planes’ meeting point, it is rich in mana. It is theoretically as expansive as our universe but is hostile to all forms of matter. Should matter enter this space unprotected, it will disappear instantaneously. However, gravity from our universe is able to penetrate space-time and enter the Void, creating areas known as "mass shadows." Due to its nature of being composed of many more spatial dimensions than the Material Plane, one location in the Void can coincide with multiple locations in the Material Plane. This property has led to the rise of a {faster-than-light} method of transport known as voidjumping."""
 
 # this is the file the html gets saved in, make sure to include
 # the .html extension
-filename = "combatmages-lore.html"
+filename = "cosmology-lore.html"
 
 # ignore this stuff
 article = article.replace('{', "<a class='infolink' href='(LINKGOESHERE)'>")
