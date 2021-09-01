f2e1d7e506a281b93f89ba6210c3999c77dda17d
Maxwell Zen <maxwell.zen@gmail.com>
2021-09-01 18:14:44 -0400

    added Paraphysics lore

--- generatelore.py
+++ generatelore.py
@@ -2,9 +2,9 @@
 
 # This will be displayed as the title of the tab, ex "Aeternum"
 # will display the title "Aeternum - Project Anima" on the tab
-tabtitle = "Mithraeho"
+tabtitle = "Paraphysics"
 
-keywords = "Mithraeho, sophont, heptapods, Dakhara, Newborn Races, Galactic Commonwealth, Mithraeho Spheres, tidally locked, nomadism, autotrophic appendages, Dakharan species, hermaphroditic (spermatophore), ramipede, hammerhead ramipedes, treader ramipedes, photoworms, zed takthakna, gluttonous ramipedes, tracker ramipedes, jeweled beetles, underground oceans, slot canyon civilizations"
+keywords = "Paraphysics, conceptual-material interactions, metaphysical-physical interactions, thaumaturgy, thaumatology, theurgy (shamanism), onturgy (oneiromancy, Reality Bending), entities, soma, anchor, soul, Conceptual Plane, Material Plane, mana, conceptual particles, information particles (infons), systems, psions, spells, rituals, psion clusters, mana production, Font, anchor generation, spellcasters, Infonic Web (infoweb), Uncertainty Principle, Variation Principle, controlled systems, uncontrolled systems, multi-targeting, multiple-psion systems, Split System Principle, mana limit, psion limit, Psionic Extension, wands, archaic, Verses (Divine Words), subjugation, Channeling, Manifestation, Host, Anchor Decay, Spontaneity, the God of Existence (the Dreamer), apotheosis, the Dream"
 
 # This is the information that will appear in the lil box on
 # the right, the order the entries appear in the box is:
@@ -12,17 +12,17 @@ keywords = "Mithraeho, sophont, heptapods, Dakhara, Newborn Races, Galactic Comm
 # 2 5
 # 3 6
 infoheadings = ["CATEGORY", "DOMAIN", "CLASS", "SPECIES", "AUTHOR", "LAST UPDATE"]
-infoentries = ["Factions", "Mithraeho Domain", "Species", "Mithraeho", "Devin Deng", "September 1st, 2021"]
+infoentries = ["Subjects", "Mixed", "Science", "Mixed", "Devin Deng", "September 1st, 2021"]
 
 # this is the address of the image that appears in the lil
 # box on the right, make sure it's relative to the html file
 # (probably something like resources/image.jpg)
-infoimagesource = "resources/Mithraeho Icon.svg"
+infoimagesource = "resources/Paraphysics Icon.svg"
 
 # if the article has links, put the addresses for those links
 # here. They must be listed in the same order as they appear
 # in the article. You can omit the .html extension
-articlelinks = ['milkywaygalaxy-lore#newborn-races', 'galacticcommonwealth-lore']
+articlelinks = ['humanspells-lore']
 
 # this is the hardest part so read it carefully, look at the
 # example for reference, make sure text is surrounded by triple quotes.
@@ -49,41 +49,66 @@ articlelinks = ['milkywaygalaxy-lore#newborn-races', 'galacticcommonwealth-lore'
 # 6 - any line that doesn't start with equal signs is interpreted
 #     as a regular paragraph
 
-article = """===MITHRAEHO
-The Mithraeho are a sophont race of heptapods originating from the nearly tidally locked planet of Dakhara. They are one of the primary {Newborn Races} of the {Galactic Commonwealth} with their current government being the Mithraeho Spheres.
-==EVOLUTION AND BIOLOGY
-=Habitat
-The planet of Dakhara was one composed of a scorching side, frozen side, and a life-filled band in between the two. It is a planet that is nearly tidally locked, meaning that it would rotate very slowly on a geological time frame. This meant that most of the life born on Dakhara had to adopt nomadism, traveling every few years to stay within the habitable zone.
-=Physiology
-The Mithraeho possess seven limbs consisting of a tail, four arms, and two legs. The tail and legs act to help maintain balance and move the Mithraeho around while the arms are used to manipulate the environment. The rear and fore structures each possess their own rudimentary brain to allow for more advanced control. Meanwhile, the primary brain is situated within the head, an elongated structure possessing three eyes and a four-part mouth. The head is mounted on a neck that is dotted with spiracles for both communication and breathing. Scattered along their backs are tubular structures that extrude growths known as “blooms”. These are essentially autotrophic appendages utilized by Mithraeho to help vent heat as well as collect energy from sunlight through photovoltaic cells. These appendages are common amongst Dakharan species, used to attract mates in ritualistic dances. While these blooms are no longer needed by Mithraeho due to their domestication of many species and the increased amounts of meat in their diet, they still act as sexual ornaments.
-=Reproduction
-Mithraeho are hermaphroditic and utilize a form of spermatophore to mate. Once the eggs are fertilized they will spend a few months formulating an egg. This egg will have a shell composed of photovoltaic cells and thus can develop on its own.
-==SOCIETY AND CULTURE
-Ancient Mithraeho societal status placed heavy emphasis on one's control over the various domesticated animals of Dakhara, with the animals serving as both currency and beasts of burden. This was due to the need to transport large amounts of people and goods during migratory periods.
-=Ramipede
-The ramipede was the center of Mithraeho civilization, its unique physiological traits leading it to be bred to fill many different niches. Mithraeho culture emphasized total usage of the carcass of a ramipede, from using its carapace as armor to its bones as building material.
-_Hammerhead
-Hammerhead ramipedes were bred to be short, stocky, and heavily armored. This led to its extensive use in clearing foliage, demolition, and as a war mount used by shock cavalry.
-_Treader
-The treader ramipede specializes in carrying loads and thus was bred for endurance. They lacked most of their armor, possessing only small scales of vestigial plating. Harnesses such as saddles and photoworm mills were mounted on the backs of these creatures. It's not uncommon to see a Mithraeho leader travel by having their hut placed on a mobile platform and having hundreds of treader ramipedes tow it. Treader ramipedes abilities and applications gained them the moniker “zed takthakna”, meaning “walking civilization”.
-_Gluttonous
-The gluttonous ramipede were the equivalent of human cattle for the Mithraeho. They were engorged on photoworms and plants and thus were forced to be quite fat. It was said that in only a few thousand years, the gluttonous ramipede went from a twenty percent average body fat to four hundred percent average body fat.
-_Tracker
-Tracker ramipedes are the smallest breed of their brethren. They had extremely strong senses of smell and speed, allowing them to run down prey for their handlers quickly.
-=Photoworms
-Photoworms were mobile autotrophs that were flat and moved by expanding and compressing themselves. Photoworm colonies often sync their crawl rates to maintain a specific position in relation to the sun, allowing for nearly non-stop crawling. To gain a height advantage, photoworms would also climb onto tall plants and other animals. Photoworms served as one of the primary food sources for Mithraeho and ramipedes and were often ground up into a jelly for preservation.
-=Jeweled Beetles
-Jeweled beetles were small, brilliantly colored insectoids that were used as a symbol of wealth and status within Mithraeho culture. Their value stemmed from both their beautiful shells as well as their difficulty to maintain. Their shells would be used to adorn clothing while live specimens would be placed in jars to act as jewelry.
-=Mating Ritual
-Mithraeho mating rituals consist of a series of rapid flapping and blooming between prospective partners. Larger and more colorful fronds would be chosen as mates due to it displaying their ability to survive in spite of their handicap.
-=Windmills
-Due to the incredibly strong winds that would flow from the hot to the cold side, Mithraeho would make extensive use of windmill technology. This often came in the form of modular photoworm grinders mounted on the backs of treader ramipides.
-=Central Civilizations
-Due to the manner in which Dakhara rotates, two opposite ends of the planet are in a state of perpetual habitability, providing ideal locations for static cities and civilizations to establish themselves. The Mithraeho people in these areas would live in deep holes and caves to protect themselves from the strong winds and storms of Dakhara. Due to the large amounts of rain, these holes would have sewer systems that drained into underground oceans, which themselves provided the Mithraeho with food and natural resources. Multiple holes would sometimes be interconnected over time via trenches and tunnels, forming slot canyon civilizations. Nomadic Mithraeho also used similar holes and canyons to act as waystations."""
+article = """===PARAPHYSICS
+Paraphysics is an umbrella term used to refer to conceptual-material (metaphysical-physical) interactions. It can be divided into three fields: thaumaturgy, theurgy, and onturgy. Each one of these fields utilizes paraphysics in different ways to different effects.
+=Entities
+To understand paraphysics, one must first understand the composition of an entity. Entities are the product of a soma, anchor, and soul coming together. In short, it is an independent organism. A living thing cannot exist without the existence of all these three. The soma is the physical body that serves as the foundation for the anchor. The anchor is the product of specific patterns of energy. When an anchor is generated, it pushes an information signature into the Conceptual Plane. This signature is known as the soul. The soul has no will of its own—it’s simply a container for memories.
+==THAUMATOLOGY
+Thaumatology is the science of using mana and conceptual particles to influence particle interactions and the art of thaumaturgy is its application. Thaumaturgy is the most widely understood and developed form of paraphysics due to every living thing possessing the potential to learn it and apply it.
+=Process
+When a thaumaturgist targets an infon or a set of infons, a “system” is designated. Psions and mana are coalesced in a spell and are directed towards the target system. For an infon to successfully be bonded and overwritten by a psion, the necessary energy threshold must be reached. This is the phase where mana is injected into the system, allowing for the psion to bind to the infon and rewrite it. The particles within the system then carry out the command of the newly rewritten infon. This process is limited by the speed of light.
+={Spells}
+Spells are information packages that are composed using a ritual, which calculates the necessary commands for the target system.
+=Reagents
+_Mana
+Mana is an energy constantly produced by the interactions between the Conceptual and Material Planes. This is most common in the form of souls, which are intrusions into the Conceptual Plane. The larger a system is, the more mana it requires for fuel. If the psion cluster contains a command that its mana partner does not have enough energy for, the bonding will not work at all. Mana is produced through a method called “intrusion”. When a being is born, its soul forces its way into the Conceptual Plane. This location is completely randomized. The closer a soul’s conceptual location is to the Font, the more mana they initially produce. The limit for mana production is theoretically infinite, but its rarity increases exponentially when a soul is to the Font. Mana production can also be influenced by age. The older a soul is, the greater its signature grows. This allows for an increased intrusion, and thus an increased rate of mana production.
+_Psions
+Psions are a byproduct of anchor generation. These particles carry the commands and calculations of the spellcaster. The number of necessary psions needed to stimulate a reaction scales proportionally with the complexity of a system. As a result, the more complex a system is, the more processing power is required.
+_Infons
+Everything in the material universe is part of something known as the Infonic Web. This Web is composed of fragments of order known as information particles (infons). It contains the information for how everything within its system is meant to interact and operate. From the smallest subatomic particle to the largest of galaxies, each one has an infon—becoming more advanced the more complicated its system is.
+=Principles and Notes
+_Uncertainty Principle
+Due to the abundance of infons in the universe, it is difficult to target a single specific infon. As a result, spells are reduced to approximations and are graded on accuracy rather than precision.
+_Variation Principle
+No two spellcasters’ spells are the same. This is due to the inherent differences in how entities perceive and process information. Certain memories can be linked to a particular spell and subconsciously influence it in some way. Spells must be regularly updated to account for changing mental states as well.
+_Imperfect Efficiency
+Thaumaturgy is not perfectly efficient—controlled systems would still be exposed to uncontrolled systems. As a result, a “system” is usually a sphere with lessening effects the further from it is from the center.
+_Multi-Targeting
+When multiple entities target the same system, infons rewrite themselves based on the most recent psion. However, in multiple-psion systems, due to the Split System Principle, the effects of the psions are coalesced, turning the systems into averages of the psion streams.
+_Mana Limit
+People can only generate a certain amount of mana at a time. Once stored mana is exhausted, and mana is being consumed faster than it can be replenished, the spells are canceled.
+_Learning Curve
+Thaumaturgy is a learned skill. One needs to understand how to generate a phenomenon in order to properly rewrite the target Infon. However, basic spells can be accessed with the infoweb.
+_Implications
+While there was much confusion concerning thaumaturgy when it was first discovered, it is now ambiguous and fundamental in modern life. However, even the most advanced scientists in the Human Domain have still not figured out what allows neural activity to produce psions, the origin of mana, and the existence the Conceptual Plane as a whole.
+_Psion Limit
+Naturally, anchors can only emit so many Psions at a given time. With the help of Psionic Extension, psions could be used more efficiently and even increased.
+_Accuracy
+Psions and mana are line-of-sight particles. This means that psions have a very small chance of intercepting a non-target Infon. On top of this, humans that aim psions unassisted are most likely going to miss. As a result, wands are used to help linearize fired psions.
+_Archaic
+Various practices used by ancient thaumaturgists have been tested and debunked through scientific analysis. One of these were Divine Words. Divine Words—otherwise known as Verses—are in actuality just a means of organizing the caster’s thoughts. The words themselves have no magical effect; they are simply a method for pacing rituals, where large numbers of casters compose a singular spell. Verses are no longer in use due to their inefficiency.
+==THEURGY (SHAMANISM)
+Theurgy is the study of anima and their effects on the material universe. Theurgists subjugate anima and use them for their own benefit.
+=Channeling
+Channeling is necessary for an anima to induce phenomena within the Material Plane. To accomplish channeling, an anima must manifest itself using an existing anchor. Manifestation is the process where an anima uses the soul of one of its constituents known as a Host as an entry point into the Material Plane. The anima and soul share the soma and anchor, forming a connection and allowing the anima’s abilities to be exhibited in the Material Plane. The more of an adherent the soul is to the anima, the more effective this connection is. However, the sharing of the anchor places strain on it. This induces Anchor Decay, which wears down and eventually destroys the anchor, thereby killing the Host. This can be decreased if the entity dedicates more of its soul towards the anima. Anchor Decay can also be prevented by strengthening the anchor. Ideally, the perfect anima-soul connection induces negligible amounts of anchor decay, and the Host is able to take full advantage of the anima’s set of abilities.
+=Forms
+An anima can serve as a “conceptual processor.” Any forms of expression, dubbed “vectors,”  can be used as a catalyst that allows the anima to generate phenomena through a Host. This means specific sounds, patterns, and symbols that are correlated with an anima can be used to produce phenomena. Such vectors include scribery, choristry, illustration, and composition.
+=Principles and Notes
+_Spontaneity
+Channeling an anima is a heavily prepared process. One must dedicate oneself to one’s anima to have the anima let one use its powers. Some might spend their entire lives worshipping or devoting themselves to an anima, yet are never able to channel its powers.
+==ONTURGY (ONEIROMANCY, REALITY BENDING)
+Onturgy is, by far, the most esoteric out of the three fields of paraphysics. This is due to its nature as a boon from the god of Existence, the Dreamer. In essence, the art of onturgy is rewriting reality. It is unknown how many possess this ability within the Milky Way Galaxy, but the current estimation rests at nine.
+=Theory
+While not much is known about onturgy, some theories have been produced to explain this phenomenon. The current prevailing theory amongst academic circles that study the phenomenon (which are rare themselves) is that reality exists upon a baseline. Every living thing experiences the baseline reality relatively unchanged. However, onturgists possess the power to generate their own sub-realities and impose them on baseline reality. However, it has been noted that onturgists can only bend reality, not change it entirely. This means they obey causality to some extent and they cannot do things like erase someone's past, as that would imply time travel.
+=Acquisition
+A being can, very rarely, achieve a state of self-imposed apotheosis. They can do this by understanding the true nature of the Dream. When they do so, they are paid a visit by the Dreamer, and are given the ability to warp reality.
+=Principles and Notes
+_Scarcity
+Reality benders are often alone and have to learn by themselves due to the rarity of the skill. Due to their solitude, they have also developed highly varied personalities, goals, and onturgical techniques."""
 
 # this is the file the html gets saved in, make sure to include
 # the .html extension
-filename = "mithraeho-lore.html"
+filename = "paraphysics-lore.html"
 
 # ignore this stuff
 
